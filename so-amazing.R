5f1e2e1ff914edf3c26f01a00a482b5ffab99267
Camille Kroot <camille.kroot@unh.edu>
2025-10-21 14:50:29 -0400

    changed back to squared

--- so-amazing.R
+++ so-amazing.R
@@ -3,10 +3,14 @@
 # my code is amazing
 
 x <- 5
-y <- x^3
+y <- x^2
 
 # git lets you take snapshots of what you are doing. Capture the moment
 # there are two steps: staging and committing
 # staging: click staged and take a photo of this one document (can stage multiple things at once)
 # then hit commit-- after committing, the file disappears because there is nothing new to snapshot. If you change the code, then save, then you can take a new snapshot
 # this acts as a "master undo" button! Nice history with different versions!
+# You can see everything under history (Git --> History)
+
+# Push --> push from local to online
+# Pull --> pull from online to local
